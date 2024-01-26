db8ac4517a97d7361e8b1a0aff42d346e1a0635c
Rohanniji <156123488+Rohanniji@users.noreply.github.com>
2024-01-26 12:46:32 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -34,6 +34,10 @@ scene.onOverlapTile(SpriteKind.Projectile, assets.tile`myTile0`, function (sprit
     tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 14))
 })
 function doSomething (num: number) {
+    Tilemaps = [tilemap`level 1`, tilemap`level3`, tilemap`level11`]
+    tiles.setCurrentTilemap(Tilemaps._pickRandom())
+    tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 13))
+    tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 13))
     if (num < 5) {
         while (FireDeaths < 2) {
             if (Player_1.tileKindAt(TileDirection.Bottom, sprites.dungeon.hazardWater)) {
@@ -44,6 +48,8 @@ function doSomething (num: number) {
         }
         if (Player_1.tileKindAt(TileDirection.Bottom, sprites.dungeon.hazardWater)) {
             game.splash("Game Over")
+            tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 13))
+            tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 13))
         }
         if (true) {
             while (WaterDeaths < 2) {
@@ -55,6 +61,8 @@ function doSomething (num: number) {
             }
             if (Player_2.tileKindAt(TileDirection.Bottom, sprites.dungeon.hazardLava0)) {
                 game.splash("Game Over")
+                tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 13))
+                tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 13))
             }
         }
     } else {
@@ -78,6 +86,7 @@ scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
         Player_2.vy = 0
     }
 })
+let Tilemaps: tiles.TileMapData[] = []
 let WaterDeaths = 0
 let FireDeaths = 0
 let jump = 0
@@ -87,9 +96,6 @@ namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 255
     export const ARCADE_SCREEN_HEIGHT = 255
 }
-doSomething(game.askForNumber("0-4 For extra life/5-9 For slower gravity", 1))
-let Tilemaps = [tilemap`level 1`, tilemap`level3`, tilemap`level11`]
-tiles.setCurrentTilemap(Tilemaps._pickRandom())
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -108,20 +114,18 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Food)
-pause(100)
 Player_1 = sprites.create(assets.image`fire boy water girl`, SpriteKind.Player)
 Player_2 = sprites.create(assets.image`fire boy water girl0`, SpriteKind.Projectile)
 scaling.scaleByPercent(Player_2, -75, ScaleDirection.Uniformly, ScaleAnchor.Middle)
 scaling.scaleByPercent(Player_1, -75, ScaleDirection.Uniformly, ScaleAnchor.Middle)
 controller.player1.moveSprite(Player_1, 100, 0)
 controller.player2.moveSprite(Player_2, 100, 0)
-tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 14))
-tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 14))
 Player_1.ay = 300
 Player_2.ay = 300
 jump = 0
 FireDeaths = 0
 WaterDeaths = 0
+doSomething(game.askForNumber("0-4 For extra life/5-9 For slower gravity", 1))
 game.onUpdate(function () {
     mySprite.setPosition((Player_1.x + Player_2.x) / 2, (Player_1.y + Player_2.y) / 2)
     scene.cameraFollowSprite(mySprite)
