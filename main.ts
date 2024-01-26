43dfcf74fcc5396a73d6cea8fc5828a544df047d
Rohanniji <156123488+Rohanniji@users.noreply.github.com>
2024-01-26 12:55:36 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -39,17 +39,11 @@ function doSomething (num: number) {
     tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 13))
     tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 13))
     if (num < 5) {
-        while (FireDeaths < 2) {
-            if (Player_1.tileKindAt(TileDirection.Bottom, sprites.dungeon.hazardWater)) {
-                game.splash("You have one more chance")
-                tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 13))
-                tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 13))
-            }
+        while (false) {
+        	
         }
         if (Player_1.tileKindAt(TileDirection.Bottom, sprites.dungeon.hazardWater)) {
-            game.splash("Game Over")
-            tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 13))
-            tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 13))
+        	
         }
         if (true) {
             while (WaterDeaths < 2) {
@@ -77,7 +71,43 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardWater, function (sp
     FireDeaths += 1
     sprites.destroy(Player_1, effects.spray, 500)
     tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 13))
+    if (FireDeaths >= 2) {
+        game.splash("Game Over")
+        tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 13))
+        tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 13))
+    }
 })
+function PlayerSetUp () {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    Player_1 = sprites.create(assets.image`fire boy water girl`, SpriteKind.Player)
+    Player_2 = sprites.create(assets.image`fire boy water girl0`, SpriteKind.Projectile)
+    scaling.scaleByPercent(Player_2, -75, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    scaling.scaleByPercent(Player_1, -75, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    controller.player1.moveSprite(Player_1, 100, 0)
+    controller.player2.moveSprite(Player_2, 100, 0)
+    Player_1.ay = 300
+    Player_2.ay = 300
+    jump = 0
+    FireDeaths = 0
+    WaterDeaths = 0
+}
 scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
     if (!(Player_2.isHittingTile(CollisionDirection.Top))) {
         jump = 0
@@ -86,9 +116,10 @@ scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
         Player_2.vy = 0
     }
 })
+let mySprite: Sprite = null
+let FireDeaths = 0
 let Tilemaps: tiles.TileMapData[] = []
 let WaterDeaths = 0
-let FireDeaths = 0
 let jump = 0
 let Player_2: Sprite = null
 let Player_1: Sprite = null
@@ -96,35 +127,7 @@ namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 255
     export const ARCADE_SCREEN_HEIGHT = 255
 }
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Food)
-Player_1 = sprites.create(assets.image`fire boy water girl`, SpriteKind.Player)
-Player_2 = sprites.create(assets.image`fire boy water girl0`, SpriteKind.Projectile)
-scaling.scaleByPercent(Player_2, -75, ScaleDirection.Uniformly, ScaleAnchor.Middle)
-scaling.scaleByPercent(Player_1, -75, ScaleDirection.Uniformly, ScaleAnchor.Middle)
-controller.player1.moveSprite(Player_1, 100, 0)
-controller.player2.moveSprite(Player_2, 100, 0)
-Player_1.ay = 300
-Player_2.ay = 300
-jump = 0
-FireDeaths = 0
-WaterDeaths = 0
+PlayerSetUp()
 doSomething(game.askForNumber("0-4 For extra life/5-9 For slower gravity", 1))
 game.onUpdate(function () {
     mySprite.setPosition((Player_1.x + Player_2.x) / 2, (Player_1.y + Player_2.y) / 2)
