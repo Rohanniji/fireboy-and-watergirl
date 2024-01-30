dfbd15a3cdd7b30e58182966f4d8f73cc5f6371c
Rohanniji <156123488+Rohanniji@users.noreply.github.com>
2024-01-30 11:08:58 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,20 +18,41 @@ scene.onOverlapTile(SpriteKind.Player, sprites.builtin.field0, function (sprite,
     tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 14))
 })
 scene.onOverlapTile(SpriteKind.Projectile, sprites.dungeon.hazardLava0, function (sprite, location) {
-    while (WaterDeaths < 2) {
-        WaterDeaths += 1
+    WaterDeaths += 1
+    while (WaterDeaths < 1) {
         sprites.destroy(Player_2, effects.spray, 500)
         game.splash("You have one more chance")
-        tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 13))
-        tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 13))
+        Movement()
+        pauseUntil(() => Player_1.tileKindAt(TileDirection.Bottom, sprites.dungeon.hazardWater) || Player_2.tileKindAt(TileDirection.Bottom, sprites.dungeon.hazardLava0))
+        game.splash("Game Over")
     }
-    game.splash("Game Over")
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (jump < 2) {
         jump += 1
         Player_2.vy = -150
     }
+})
+function Movement () {
+    Player_1 = sprites.create(assets.image`fire boy water girl`, SpriteKind.Player)
+    Player_2 = sprites.create(assets.image`fire boy water girl0`, SpriteKind.Projectile)
+    scaling.scaleByPercent(Player_2, -75, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    scaling.scaleByPercent(Player_1, -75, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    controller.player1.moveSprite(Player_1, 100, 0)
+    controller.player2.moveSprite(Player_2, 100, 0)
+    tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 14))
+    tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 14))
+    if (mySprite == 1) {
+        Player_1.ay = 300
+        Player_2.ay = 300
+    } else {
+        Player_1.ay = 200
+        Player_2.ay = 200
+    }
+}
+scene.onOverlapTile(SpriteKind.Projectile, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`myTile`)
+    info.changeScoreBy(1)
 })
 scene.onOverlapTile(SpriteKind.Projectile, sprites.builtin.field0, function (sprite, location) {
     game.splash("Game Over")
@@ -40,6 +61,7 @@ scene.onOverlapTile(SpriteKind.Projectile, sprites.builtin.field0, function (spr
 })
 function doSomething (num: number) {
     if (num < 5) {
+        mySprite = 1
         Player_1.ay = 300
         Player_2.ay = 300
         Tilemaps = [tilemap`level 1`, tilemap`level3`, tilemap`level11`]
@@ -47,8 +69,9 @@ function doSomething (num: number) {
         tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 14))
         tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 14))
     } else {
-        Player_1.ay = 225
-        Player_2.ay = 225
+        mySprite = 2
+        Player_1.ay = 200
+        Player_2.ay = 200
         Tilemaps = [tilemap`level 1`, tilemap`level3`, tilemap`level11`]
         tiles.setCurrentTilemap(Tilemaps._pickRandom())
         tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 14))
@@ -63,9 +86,10 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardWater, function (sp
     while (FireDeaths < 2) {
         sprites.destroy(Player_1, effects.spray, 500)
         game.splash("You have one more chance")
-        doSomething2()
+        Movement()
+        pauseUntil(() => Player_1.tileKindAt(TileDirection.Bottom, sprites.dungeon.hazardWater) || Player_2.tileKindAt(TileDirection.Bottom, sprites.dungeon.hazardLava0))
+        game.splash("Game Over")
     }
-    game.splash("Game Over")
 })
 scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
     if (!(Player_2.isHittingTile(CollisionDirection.Top))) {
@@ -75,17 +99,12 @@ scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
         Player_2.vy = 0
     }
 })
-function doSomething2 () {
-    Player_1 = sprites.create(assets.image`fire boy water girl`, SpriteKind.Player)
-    Player_2 = sprites.create(assets.image`fire boy water girl0`, SpriteKind.Projectile)
-    scaling.scaleByPercent(Player_2, -75, ScaleDirection.Uniformly, ScaleAnchor.Middle)
-    scaling.scaleByPercent(Player_1, -75, ScaleDirection.Uniformly, ScaleAnchor.Middle)
-    controller.player1.moveSprite(Player_1, 100, 0)
-    controller.player2.moveSprite(Player_2, 100, 0)
-    tiles.placeOnTile(Player_1, tiles.getTileLocation(15, 14))
-    tiles.placeOnTile(Player_2, tiles.getTileLocation(14, 14))
-}
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`myTile`)
+    info.changeScoreBy(1)
+})
 let Tilemaps: tiles.TileMapData[] = []
+let mySprite = 0
 let WaterDeaths = 0
 let FireDeaths = 0
 let jump = 0
@@ -102,6 +121,7 @@ scaling.scaleByPercent(Player_1, -75, ScaleDirection.Uniformly, ScaleAnchor.Midd
 controller.player1.moveSprite(Player_1, 100, 0)
 controller.player2.moveSprite(Player_2, 100, 0)
 doSomething(game.askForNumber("You get 2 lives each character! Press 5-9 for extra help", 1))
+info.setScore(0)
 jump = 0
 FireDeaths = 0
 WaterDeaths = 0
